4607154d17f10b50cb3e4ba1426c3f4ec8bd637a
EgorIK55 <mat.matveyaka@gmail.com>
2024-05-12 13:02:09 +0700

    awsdft

--- src/main/kotlin/ru/dinosaurus/problemsserver/models/FirstPageData.kt
+++
@@ -1,5 +0,0 @@
-package ru.dinosaurus.problemsserver.models
-
-data class FirstPageData(
-    val title:String = "Lingvistic"
-)
--- src/main/kotlin/ru/dinosaurus/problemsserver/repositories/ReadersRepository.kt
+++
@@ -1,7 +0,0 @@
-package ru.dinosaurus.problemsserver.repositories
-
-import org.springframework.data.jpa.repository.JpaRepository
-import ru.dinosaurus.problemsserver.models.BooksReader
-
-
-interface ReadersRepository: JpaRepository<BooksReader?,Long?>
